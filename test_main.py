09d6d982a2d1e97624d353b6dbed5ab9896b8be6
Dylan V <153835867+dylanvivant@users.noreply.github.com>
2025-05-12 16:25:34 +0200

    Test avec plusieurs paramètres

--- test_main.py
+++ test_main.py
@@ -3,18 +3,5 @@ import time
 from main import hello
 
 
-def test_hello_type_error():
-    with pytest.raises(TypeError):
-        hello(123)
-
-
-def test_hello_performance():
-    start = time.time()
-    for _ in range(1000):
-        hello('EPSI')
-    duration = time.time() - start
-    assert duration < 0.1
-
-
 def test_hello_full_name():
     assert hello(firstname="John", lastname="Doe") == "Hello, John Doe!"
